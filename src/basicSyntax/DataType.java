14e119e15255dad42bfbbe54fdca2ece7ae7684e
Narumi Namba <narunomoto1030@gmail.com>
2025-04-22 18:53:20 +0900

    課題の提出

---
+++ src/basicSyntax/DataType.java
@@ -0,0 +1,24 @@
+package basicSyntax;
+
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String Hello = "こんにちは";
+		String MyName = "私の名前は";
+		String Is = "Geek君です。";
+		System.out.println(Hello + MyName + Is);
+		
+		int a = 10;
+		int b = 2024;
+		double c = 3.14;
+		boolean True = true;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(True);
+	}
+
+}
